9a08ebca3b9f73c219e8b94946e719a9f10caa2b
joaovitor774 <joaovitordavy76@gmail.com>
2026-02-09 12:45:42 -0400

    Primeiras atividades em Ruby

---
+++ aula4.rb
@@ -0,0 +1,14 @@
+#1)Saida de dados na tela --> aparecer um comando na tela
+# usuário digite seu nome
+print "Digite seu nome :  "
+
+#2) programa permita que usuario preencha o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+print "Digite deu sobrenome: "
+sobrenome = gets.chomp
+
+#3) saida final
+# para chamar um dado #{variavel}
+puts "oi #{nome} #{sobrenome} !"
---
+++ aula4soma.rb
@@ -0,0 +1,12 @@
+#Usuário digite 1 numero
+#pedir ao usuário
+#.to_i
+print "Digite um número inteiro: "
+num1 = gets.chomp.to_i
+#Usuário digite outro número
+print "Digite outro número inteiro: "
+num2 = gets.chomp.to_i
+#Some essses números do usuário
+soma = num1 + num2
+#apareça na tela
+puts "O resultado da soma será: #{soma}"
