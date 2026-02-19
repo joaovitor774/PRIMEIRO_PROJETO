d53935d5986ba4a124cf22a2ee59901484bc20de
joaovitor774 <joaovitordavy76@gmail.com>
2026-02-19 11:15:04 -0400

    Organizando o projeto em estrutura de estudos Ruby

---
+++ 01_fundamentos/metodos/metodos.rb
@@ -0,0 +1,9 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+def nome (nome,sobrenome)
+    puts "oi #{nome} #{sobrenome}, você é um Dev em RUby!"
+end
+nome(nome,sobrenome)
